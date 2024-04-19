ca20f41d58c930aa7cd5a235b441ccb0ec3d4af8
Nicolas Marshall <marshall.nicolas@gmail.com>
2024-04-19 10:01:11 +0100

    import flake

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rust-libs
 Rust libraries
+
+
+
+
+# Contributing
+
+License: [Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0)
---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "utils";
+    };
+    # crane = {
+    #   url = "github:ipetkov/crane";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+   my-utils={url = "github:nmrshll/nix-utils";      
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+      # inputs.rust-overlay.follows = "rust-overlay";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, utils, my-utils }:
+    with builtins; utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+          customRust = pkgs.rust-bin.stable."1.76.0".default.override {
+            extensions = [ "rust-src" "rust-analyzer" ];
+            targets = [ ];
+          };
+
+          buildDependencies = with pkgs; [
+            customRust
+            nodePackages_latest.pnpm
+          ] ++ (
+            lib.optionals stdenv.isDarwin [
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.SystemConfiguration
+              darwin.apple_sdk.frameworks.CoreServices
+              darwin.apple_sdk.frameworks.CoreFoundation
+              darwin.apple_sdk.frameworks.Foundation
+              libiconv
+            ]
+          );
+          devDependencies = [
+            pkgs.cargo-edit
+            pkgs.watchexec
+          ];
+
+          env = rec {
+            RUST_LOG = "debug";
+            RUST_BACKTRACE = 1;
+          };
+
+          binaries = my-utils.binaries.${system};
+          scripts = attrValues my-utils.packages.${system} ++ [
+            # (pkgs.writeScriptBin "back" ''cargo run'')
+          ];
+
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = buildDependencies ++ devDependencies ++ scripts;
+            shellHook = ''
+              ${binaries.configure-vscode}; 
+              dotenv
+            '';
+            inherit env;
+          };
+        }
+      );
+}
